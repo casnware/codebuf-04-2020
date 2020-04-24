414d35180d3f98d10da5a9272deed1df666b0133
mcdanije22 <mcdanije22@gmail.com>
2020-04-24 17:22:16 -0400

    front end work on home screen, customer screens, login screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './components/landing/LandingPage';
+import HomePage from './components/home/HomePage';
+import Login from './components/login/Login';
+import { LanguageContext } from './context/LanguageContext';
 
 const App = () => {
-  return <div className="App"></div>;
+  const [currentLanguage, setLanguage] = useState(null);
+  return (
+    <LanguageContext.Provider value={{ currentLanguage, setLanguage }}>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={HomePage} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </LanguageContext.Provider>
+  );
 };
 
 export default App;
---
+++ client/src/components/home/HomePage.js
@@ -0,0 +1,127 @@
+import React, { useContext, useState } from 'react';
+import './homePage.scss';
+import { LanguageContext } from '../../context/LanguageContext';
+import { Layout, Menu, Collapse, Button } from 'antd';
+import { MenuOutlined, LeftOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+const { Header, Footer, Sider, Content } = Layout;
+const { Panel } = Collapse;
+const { SubMenu } = Menu;
+
+const HomePage = () => {
+  const { currentLanguage } = useContext(LanguageContext);
+  const [isOpen, toggleMenu] = useState(false);
+
+  const openMenu = () => {
+    toggleMenu(isOpen ? false : true);
+  };
+  return (
+    <div id="HomePageContainer">
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          style={{ backgroundColor: 'white' }}
+          breakpoint="md"
+          collapsedWidth="0"
+        >
+          <Menu mode="inline">
+            <Menu.Item key="6">
+              <Link to="/">
+                <span className="nav-text">
+                  <LeftOutlined />
+                  Choose a Language
+                </span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="1">
+              <span className="nav-text">Store Info</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <span className="nav-text">Pharmacy</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <span className="nav-text">Home</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <span className="nav-text">Eat In</span>
+            </Menu.Item>
+            <Menu.Item key="5">
+              <span className="nav-text">Common Questions</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          {/* <Header style={{ backgroundColor: 'white' }} breakpoint="lg">
+            <Button style={{ border: 'none' }} onClick={openMenu}>
+              <MenuOutlined />
+            </Button>
+          </Header> */}
+          <Content
+            style={{
+              margin: '24px 16px 0',
+              backgroundColor: 'white',
+              padding: '2rem',
+            }}
+          >
+            <Collapse>
+              <Panel header="This is panel header 1" key="1">
+                <Collapse>
+                  <Panel header="This is panel header 1" key="1">
+                    <p>text</p>
+                  </Panel>
+                  <Panel header="This is panel header 2" key="2">
+                    <p>text</p>
+                  </Panel>
+                  <Panel header="This is panel header 3" key="3">
+                    <p>text</p>
+                  </Panel>
+                </Collapse>
+              </Panel>
+              <Panel header="This is panel header 2" key="2">
+                <Collapse>
+                  <Panel header="This is panel header 1" key="1">
+                    <p>text</p>
+                  </Panel>
+                  <Panel header="This is panel header 2" key="2">
+                    <p>text</p>
+                  </Panel>
+                  <Panel header="This is panel header 3" key="3">
+                    <p>text</p>
+                  </Panel>
+                </Collapse>
+              </Panel>
+              <Panel header="This is panel header 3" key="3">
+                <Collapse>
+                  <Panel header="This is panel header 1" key="1">
+                    <p>text</p>
+                  </Panel>
+                  <Panel header="This is panel header 2" key="2">
+                    <p>text</p>
+                  </Panel>
+                  <Panel header="This is panel header 3" key="3">
+                    <p>text</p>
+                  </Panel>
+                </Collapse>
+              </Panel>
+              <Panel header="This is panel header 4" key="4">
+                <Collapse>
+                  <Panel header="This is panel header 1" key="1">
+                    <p>text</p>
+                  </Panel>
+                  <Panel header="This is panel header 2" key="2">
+                    <p>text</p>
+                  </Panel>
+                  <Panel header="This is panel header 3" key="3">
+                    <p>text</p>
+                  </Panel>
+                </Collapse>
+              </Panel>
+            </Collapse>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Trazzle ©2020</Footer>
+        </Layout>
+      </Layout>
+    </div>
+  );
+};
+export default HomePage;
